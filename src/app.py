10aca5773aebfbb7f7f5e4c8bade91337513d775
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-13 19:01:17 +0700

    feat: add main part to app.py

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import uvicorn
+import os
+
+load_dotenv()
 
 app = FastAPI()
-
 
 
 @app.get("/")
 def healthCheck():
     return {"status": True}
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.environ.get("HOST"), port=int(os.environ.get("PORT")))
