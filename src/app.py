32362b23fee434109f18a13a9577936dada45de0
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-13 19:26:46 +0700

    feat: add flake8 config

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+
+# from starlette.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import uvicorn
 import os
