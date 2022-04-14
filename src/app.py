d1c144704285d8204f9725e8f043cdb323487df2
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-15 00:50:48 +0700

    feat: update ci.yml

--- src/app.py
+++ src/app.py
@@ -12,11 +12,19 @@ load_dotenv()
 app = FastAPI()
 
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@app.get(
+    "/", status_code=status.HTTP_200_OK, response_description="Welcome to MySK API"
+)
 def healthCheck(response: Response):
     response.headers["X-Internal-Code"] = str(InternalCode.IC_GENERIC_SUCCESS.value)
     return {"status": True}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=os.environ.get("HOST"), port=int(os.environ.get("PORT")))
+    uvicorn.run(
+        "app:app",
+        host=os.environ.get("HOST"),
+        port=int(os.environ.get("PORT")),
+        log_level="info",
+        reload=True,  # reload on code changes
+    )
